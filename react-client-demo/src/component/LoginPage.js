aec4372ce146d037e72284399c68408b13541355
fangyh1994 <31147954+fangyh1994@users.noreply.github.com>
2018-07-06 16:04:29 -0400

    delete nodemudules

--- react-client-demo/src/component/LoginPage.js
+++ react-client-demo/src/component/LoginPage.js
@@ -14,8 +14,8 @@ class LoginPage extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(e){
-    const {name, value} = e.target;
-    this.setState({[name]:value});
+    console.log(e.target.value);
+    this.setState({ [e.target.name] : e.target.value });
   }
   handleSubmit(e){
     e.preventDefault();
@@ -32,18 +32,18 @@ class LoginPage extends Component {
         <div className="col-md-6 col-md-offset-3">
           <h2>Login</h2>
           <form name="form" onSubmit={this.handleSubmit}>
-              <div className={'form-group' + (this.state.submitted && !this.state.username ? ' has-error' : '')}>
+              <div >
                   <label htmlFor="username">Username</label>
                   <input type="text" className="form-control" name="username" value={this.state.username} onChange={this.handleChange} />
                   {this.state.submitted && !this.state.username &&
-                      <div className="help-block">Username is required</div>
+                      <div >Username is required</div>
                   }
               </div>
-              <div className={'form-group' + (this.state.submitted && !this.state.password ? ' has-error' : '')}>
+              <div >
                   <label htmlFor="password">Password</label>
                   <input type="password" className="form-control" name="password" value={this.state.password} onChange={this.handleChange} />
                   {this.state.submitted && !this.state.password &&
-                      <div className="help-block">Password is required</div>
+                      <div >Password is required</div>
                   }
               </div>
               <div className="form-group">
